5fe5cff48631abe3a9baeae181cf11362947f8d6
吾行误述 <49816732+wuxingwushu@users.noreply.github.com>
2023-06-09 21:58:44 +0800

    类的回调函数（但是不好用）

---
+++ ClassCallback/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <functional>
+
+class MyClass {
+public:
+    void memberFunction(int arg) {
+        std::cout << "Member function called with argument " << arg << std::endl;
+    }
+};
+
+void callback(std::function<void(int)> func, int arg) {
+    func(arg);
+}
+
+int main() {
+    MyClass obj;
+
+    // 使用 bind 函数将指针和类实例组合成一个 functor
+    auto func = std::bind(&MyClass::memberFunction, &obj, std::placeholders::_1);
+
+    // 回调 functor
+    callback(func, 123);
+
+    return 0;
+}
